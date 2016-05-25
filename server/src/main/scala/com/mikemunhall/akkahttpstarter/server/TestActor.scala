68041fb59370ec082718ab9e76b37ceac76fe0e8
Mike Munhall <munhall.mike@gmail.com>
2016-05-25 09:59:52 -0600

    Simple metrics logger.

--- server/src/main/scala/com/mikemunhall/akkahttpstarter/server/TestActor.scala
+++ server/src/main/scala/com/mikemunhall/akkahttpstarter/server/TestActor.scala
@@ -2,9 +2,12 @@ package com.mikemunhall.akkahttpstarter.server
 
 import akka.actor.{Actor, Props, Stash}
 import com.typesafe.scalalogging.StrictLogging
+import kamon.Kamon
+import kamon.akka.ActorMetrics
 
 object TestActor {
   def props = Props[TestActor]
+  val counter = Kamon.metrics.counter("test-actor-counter")
 }
 
 class TestActor extends Actor with Stash with StrictLogging {
@@ -24,9 +27,11 @@ class TestActor extends Actor with Stash with StrictLogging {
 
   def initialized: Receive = {
     case "fail" =>
+      TestActor.counter.increment()
       logger.debug(s"""TestActor received message "fail"""")
       sender() ! new Exception("Fail conditions met")
     case str: String =>
+      TestActor.counter.increment()
       logger.debug(s"""TestActor received message "$str"""")
       sender() ! s"$str back at ya."
   }
